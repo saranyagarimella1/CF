2cd1b692d4b4b609e3e21aa961b94f4061a2e36a
Saranya Garimella <saranya.garimella@gmail.com>
2017-03-02 15:20:31 -0800

    Added new files

---
+++ server/src/main/java/io/saranya/CinemaFlix/api/entity/Comment.java
@@ -0,0 +1,26 @@
+package io.saranya.CinemaFlix.api.entity;
+
+public class Comment {
+
+	private String imdbId;
+	private String username;
+	private String comment;
+	public String getImdbId() {
+		return imdbId;
+	}
+	public void setImdbId(String imdbId) {
+		this.imdbId = imdbId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+}
---
+++ server/src/main/java/io/saranya/CinemaFlix/api/entity/Movies.java
@@ -0,0 +1,110 @@
+package io.saranya.CinemaFlix.api.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+@Entity
+@NamedQueries({
+	@NamedQuery(name="Movie.findAll", query="SELECT m FROM Movies m")
+	})
+public class Movies {
+
+	private String title;
+	private String rated;
+	private int year;
+	private Date released;
+	private String runtime;
+	private String genre; // lets try putting Enum type later
+	private String director;
+	private String actors;
+	private String plot;
+	private String language;
+	private float imdbRating;
+	private int imdbVotes;
+	private String imdbId;
+	// private Image poster; future!
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getRated() {
+		return rated;
+	}
+	public void setRated(String rated) {
+		this.rated = rated;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public Date getReleased() {
+		return released;
+	}
+	public void setReleased(Date released) {
+		this.released = released;
+	}
+	public String getRuntime() {
+		return runtime;
+	}
+	public void setRuntime(String runtime) {
+		this.runtime = runtime;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	public String getActors() {
+		return actors;
+	}
+	public void setActors(String actors) {
+		this.actors = actors;
+	}
+	public String getPlot() {
+		return plot;
+	}
+	public void setPlot(String plot) {
+		this.plot = plot;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	public float getImdbRating() {
+		return imdbRating;
+	}
+	public void setImdbRating(float imdbRating) {
+		this.imdbRating = imdbRating;
+	}
+	public int getImdbVotes() {
+		return imdbVotes;
+	}
+	public void setImdbVotes(int imdbVotes) {
+		this.imdbVotes = imdbVotes;
+	}
+	public String getImdbId() {
+		return imdbId;
+	}
+	public void setImdbId(String imdbId) {
+		this.imdbId = imdbId;
+	}
+	
+	
+}
+
